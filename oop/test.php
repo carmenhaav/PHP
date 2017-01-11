ef8f6b0b0ce49818913ae16597e3e98d444cce25
carmenhaav <carmen.haav@khk.ee>
2017-01-11 15:02:25 +0200

    Test for text classes

---
+++ oop/test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carmen.haav
+ * Date: 11.01.2017
+ * Time: 14:46
+ */
+// require the object creating and using a class
+require_once('text.php');
+
+// create an object
+$sentence = new text();
+
+// control object output
+echo '<pre>';
+print_r($sentence);
+echo '<pre>';
+
+// set text
+$sentence->setText('Hello, text object!');
+
+// control object output
+echo '<pre>';
+print_r($sentence);
+echo '<pre>';
+
+// show object output
+$sentence->show();
+?>
