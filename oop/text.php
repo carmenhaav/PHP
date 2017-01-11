25d1f084586298a48b8eb169f104e3652e142573
carmenhaav <carmen.haav@khk.ee>
2017-01-11 14:45:32 +0200

    Text class initial version

---
+++ oop/text.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: carmen.haav
+ * Date: 11.01.2017
+ * Time: 14:22
+ */
+class text
+{// text class begin
+	// class variables = instance variables
+	var $str = '';
+
+	// methods
+	// set text function
+	function setText($s){
+		$this->str = $s;
+	}// setText
+
+	// show text function
+	function show(){
+		echo $this->str.'<br/>';
+	}// show
+
+}// text class end
+?>
