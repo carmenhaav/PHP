bd1e13cfab6ab46e555ac60daa8cc597957faa01
carmenhaav <carmen.haav@khk.ee>
2017-01-12 13:06:44 +0200

    Create index file template

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// index.php
+/**
+ * Created by PhpStorm.
+ * User: carmen.haav
+ * Date: 12.01.2017
+ * Time: 12:58
+ */
+// create and template object
+define ('CLASSES_DIR', 'classes/');
+define ('TMPL_DIR', 'tmpl/');
+require_once CLASSES_DIR.'template.php';
+// and use it
+
+// create and empty template object
+$tmpl = new template();
+//  set up file name for the template
+$tmpl->file = 'main.html';
+// control the content of the template object
+echo '<pre>';
+print_r($tmpl);
+echo '<pre>';
+// load file content of template
+$tmpl->loadFile();
+// control the content of the template object
+echo '<pre>';
+print_r($tmpl);
+echo '<pre>';
+?>
