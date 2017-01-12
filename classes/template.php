d321424fb587fa7e0f95bb0d0990b5063466a66c
carmenhaav <carmen.haav@khk.ee>
2017-01-12 12:55:34 +0200

    Create template initial version

--- classes/template.php
+++ classes/template.php
@@ -18,9 +18,46 @@ class template
 	var $content = false; // template content - now is empty
 
 	// class methods
+
+	/**
+	 *
+	 */
+	function loadFile(){
+		$f = $this->file; // use file name variable
+		// if problems with tmpl directory
+		if(!is_dir(TMPL_DIR)){
+			echo 'Kataloogi'.TMPL_DIR.'ei ole leitud.<br/>';
+			exit;
+		}
+
+		// if we're in tmpl directory - $this->file is 'tmpl/file.html'
+		if(file_exists($f) and is_file($f) and is_readable($f)){
+			$this->readFile($f);
+		}
+
+		// set the path to template file: tmpl/file.html, $this->file is file.html
+		$f = TMPL_DIR.$this->file;
+		if(file_exists($f) and is_file($f) and is_readable($f)){
+			$this->readFile($f);
+		}
+
+		// set only the file name, $this->file is file
+		$f = TMPL_DIR.$this->file.'.html';
+		if(file_exists($f) and is_file($f) and is_readable($f)){
+			$this->readFile($f);
+		}
+
+		//
+		if($this->content === false){
+			echo 'Ei saanud lugeda faili.'.$this->file.<br/>;
+			exit;
+		}
+
+	} // loadFile
+
 	function readFile($f){
-		
-	}
+		$this->content = file_get_contents($f);
+	} // readFile
 
 }// class end
 
