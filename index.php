1cdc782dc60965e5721bb8c4d07b2e9f4d36a513
carmenhaav <carmen.haav@khk.ee>
2017-01-13 10:42:40 +0200

    Add pairs of template element names and real values

--- index.php
+++ index.php
@@ -14,6 +14,12 @@ require_once CLASSES_DIR.'template.php';
 // create an empty template object
 $tmpl = new template('main');
 
+//add pair of template element names and real values
+$tmpl->set('menu', 'minu menüü');
+$tmpl->set('nav_bar', 'minu navigatsioon');
+$tmpl->set('lang_bar', 'minu keeleriba');
+$tmpl->set('content', 'minu sisu');
+
 // control the content of the template object
 echo '<pre>';
 print_r($tmpl);
