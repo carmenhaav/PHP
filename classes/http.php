e29b44310d44e5184aaa3b88bb745141ec71cbf8
carmenhaav <carmen.haav@khk.ee>
2017-01-17 13:08:28 +0200

    Create http class initial

---
+++ classes/http.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carmen.haav
+ * Date: 17.01.2017
+ * Time: 12:48
+ */
+class http
+{// http begin
+	// class variables
+	var $_SERVER = array(); // server data
+	var $vars = array(); // http data
+	var $cookie = array(); // cookie data
+	//class methods
+
+	// construct
+	// object creation and initializing by init() and initConst() methods
+	function construct() {
+		$this->init();
+		$this->initConst()
+	}
+
+
+	// initialize class variables
+	/**
+	 *
+	 */
+	function init() {
+		$this->server = $_SERVER; // server real data
+		$this->cookie = $_COOKIE; // cookie real data
+		$this->vars = array_merge($_GET, $_POST, $_FILES); // http real data
+	} // init
+	// initalize class constants
+	function initConst() {
+		$vars = array('REMOTE_ADDR', 'PHP_SELF', 'SCRIPT_NAME', 'HTTP_HOST');
+		foreach ($vars as $var) {
+			if(!defined($var)) {
+				define($var, $this->server[$var])
+			}
+		}
+	}
+}// http end
+
+?>
--- index.php
+++ index.php
@@ -9,21 +9,25 @@
 // create and template object and use it
 define ('CLASSES_DIR', 'classes/');
 define ('TMPL_DIR', 'tmpl/');
+define ('STYLE_DIR', 'css/');
 require_once CLASSES_DIR.'template.php';
 
 // create an empty template object
 $tmpl = new template('main');
 
 //add pair of template element names and real values
+$tmpl->set('style', STYLE_DIR.'style.css');
+
+$tmpl->set('header', 'minu lehe pealkiri');
 $tmpl->set('menu', 'minu menüü');
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
-
+/*
 // control the content of the template object
 echo '<pre>';
 print_r($tmpl);
 echo '<pre>';
-
+*/
 echo $tmpl->parse();
 ?>
