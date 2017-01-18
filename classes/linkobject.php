f0692057725d4c59856acc45c61d90183c7e0deb
carmenhaav <carmen.haav@khk.ee>
2017-01-18 08:51:22 +0200

    create an initial version of linkobject class

---
+++ classes/linkobject.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: carmen.haav
+ * Date: 18.01.2017
+ * Time: 8:48
+ */
+
+// only for testing
+// import http class
+require_once 'http.php';
+// only for testing
+class linkobject extends http
+{
+
+}
+
+?>
