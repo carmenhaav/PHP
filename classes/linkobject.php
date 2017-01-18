bc196828634a9fc497a2c0b6f3ceefbddf2da737
carmenhaav <carmen.haav@khk.ee>
2017-01-18 09:30:44 +0200

    Create url data pairs

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -7,10 +7,16 @@
  * Time: 8:48
  */
 
+// useful function for this class
+function fixUrl($val) {
+	return urldecode($val);
+} // fixUrl
+
 // only for testing
 // import http class
 require_once 'http.php';
 // only for testing
+
 class linkobject extends http
 { // class begin
 	// class variables
@@ -22,11 +28,17 @@ class linkobject extends http
 	// class methods to help to make the url link
 	// construct
 	// create base URL: http//XXX.XXX.XXX.XXX/path_to_file.php
-	function __construct()
-	{
+	function __construct() {
 		parent::__construct();
 		$this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
 	} // construct
+
+	// create http data pairs and merge them
+	function addToLink($link, $name, $val) {
+		// create pair: element_name = element_value
+		$link = $link.fixUrl($name).$this->eq.fixUrl($val);
+		echo $link;
+	}// addTo link
 } // class end
 
 ?>
