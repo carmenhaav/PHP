652292caf849053524694e661abe9c54b869116d
carmenhaav <carmen.haav@khk.ee>
2017-01-19 13:03:51 +0200

    Import database configuration and use it in database object

--- conf.php
+++ conf.php
@@ -18,12 +18,13 @@ define('DEFAULT_ACT', 'default'); // default act file name
 require_once CLASSES_DIR.'template.php'; // import template class
 require_once CLASSES_DIR.'http.php'; // import http class
 require_once CLASSES_DIR.'linkobject.php'; // import linkobject class
-
 require_once CLASSES_DIR.'mysql.php'; // import database class
+
+require_once 'db_conf.php'; // import database configuration
 
 // create linkobject object
 $http = new linkobject();
 
 // create database object
-$db = new mysql('localhost', 'user', 'parool', 'dbname');
+$db = new mysql(DBHOST, DBUSER, DBPASS, DBNAME);
 ?>
