e89f56498b50037a411aadc400e0f8d92106854f
carmenhaav <carmen.haav@khk.ee>
2017-01-30 13:02:06 +0200

    redirect http data

--- classes/http.php
+++ classes/http.php
@@ -70,5 +70,18 @@ class http
 			unset($this->vars[$name]);
 		}
 	} // del
+
+	// redirect http data
+	function redirect($url = false){
+		global $sess;
+		$sess->flush();
+
+		if ($url == false){
+			$url = $this->getLink();
+		}
+		$url = str_replace('&amp;', '&', $url);
+		header('Location: '.$url);
+		exit;
+	}// redirect
 }// http end
 ?>
