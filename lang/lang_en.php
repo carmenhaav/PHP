7fac79e75578ddcda3570a44b7b6d1f2725c25d8
carmenhaav <carmen.haav@khk.ee>
2017-02-06 10:51:11 +0200

    add lang_en.php file

---
+++ lang/lang_en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carmen.haav
+ * Date: 6.02.2017
+ * Time: 10:46
+ */
+// lang/lang_en.php
+$_trans = array(
+	'Esimene' => 'First',
+	'Teine' => 'Second',
+	'Minu lehe pealkiri' => 'My page heading',
+	'Logi v&auml;ja' => 'Logout',
+	'Kasutajanimi' => 'Username',
+	'Parool' => 'Password',
+	'Logi sisse' => 'Login',
+	'Viga sisselogimisel' => 'Login error'
+);
+?>
